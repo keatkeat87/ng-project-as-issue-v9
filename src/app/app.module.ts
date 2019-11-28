49c3d990a1f2025edf9f683fe515be60752f48a9
兴杰 严 <keatkeat87@live.com>
2019-11-28 12:34:34 +0800

    ok

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AbcComponent } from './abc/abc.component';
+import { XyzComponent } from './xyz/xyz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AbcComponent,
+    XyzComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
